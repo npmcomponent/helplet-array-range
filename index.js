5594c5e2271270b07dcf134fa4d8da91d74ca5ec
Jan Buschtöns <buschtoens@gmail.com>
2013-08-30 09:30:01 +0200

    ze code

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * Expose `range`.
+ */
+module.exports = range;
+
+
+function range(from, to, steps) {
+  switch(arguments.length) {
+    case 1:
+      to = from;
+      from = 0;
+      steps = 1;
+      break;
+    case 2:
+    case 3:
+      from = from || 0;
+      to = to || 0;
+      steps = Math.abs(steps) || 1;
+      break;
+    default:
+      throw new Error("Cannot call `range(from, to, steps)` with " + arguments.length + " arguments.");
+  }
+
+  var inverse = from > to
+    , arr = this instanceof Array ? this : [];
+  for(var i = from; inverse ? i > to : i < to; inverse ? i -= steps : i += steps)
+    arr.push(i);
+
+  return arr;
+}
+
+function test(from, to, steps) {
+  console.log({ from: from, to: to, steps: steps}, range.apply(null, arguments));
+}
+
+test(0, -10, 2)
